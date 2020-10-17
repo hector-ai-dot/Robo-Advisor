38ceb18e11f1b2c4f121e6085c6f2741a7c9f05c
Kun Chen <kchen@csie.org>
2020-10-17 23:34:26 +0800

    鑒定策略函式完成

--- assets/js/wizard.js
+++ assets/js/wizard.js
@@ -1,59 +1,59 @@
 var Strategy = [{
-        name: 'Low Volatility',
-        factor: [{
-                name: "inputMIS-EXP_PCT",
-                value: [1, 2, 3]
-            },
-            {
-                name: "inputRENT_PCT",
-                value: [1, 2, 3]
-            },
-            {
-                name: "inputMoney",
-                value: [3, 4]
-            },
-            {
-                name: "inputMDD",
-                value: [1, 2]
-            },
-            {
-                name: "inputReturn",
-                value: [1, 2, 3]
-            },
-        ]
+    name: 'Low Volatility',
+    factor: [{
+        name: "inputMIS-EXP_PCT",
+        value: [1, 2, 3]
     },
     {
-        name: 'Growth',
-        factor: [{
-                name: "inputBouns_PCT",
-                value: [1, 2, 3]
-            },
-            {
-                name: "inputMDD",
-                value: [3, 4]
-            },
-            {
-                name: "inputReturn",
-                value: [3, 4]
-            },
-            {
-                name: "inputDVD",
-                value: [1, 2]
-            },
-        ]
+        name: "inputRENT_PCT",
+        value: [1, 2, 3]
     },
     {
-        name: 'Dividend',
-        factor: [{
-                name: "inputMoney",
-                value: [3, 4]
-            },
-            {
-                name: "inputDVD",
-                value: [3, 4]
-            },
-        ]
+        name: "inputMoney",
+        value: [3, 4]
     },
+    {
+        name: "inputMDD",
+        value: [1, 2]
+    },
+    {
+        name: "inputReturn",
+        value: [1, 2, 3]
+    },
+    ]
+},
+{
+    name: 'Growth',
+    factor: [{
+        name: "inputBouns_PCT",
+        value: [1, 2, 3]
+    },
+    {
+        name: "inputMDD",
+        value: [3, 4]
+    },
+    {
+        name: "inputReturn",
+        value: [3, 4]
+    },
+    {
+        name: "inputDVD",
+        value: [1, 2]
+    },
+    ]
+},
+{
+    name: 'Dividend',
+    factor: [{
+        name: "inputMoney",
+        value: [3, 4]
+    },
+    {
+        name: "inputDVD",
+        value: [3, 4]
+    },
+    ]
+},
 ]
 
 // 檢查是否所有必填欄位都有填到
@@ -72,8 +72,31 @@ var checkAllField = () => {
 }
 
 var IdentifyStrategy = () => {
-    Strategy.forEach((e) => {
-        console.log(e)
+    $('#Strategy').show()
+    Strategy.every((e) => {
+        let factorCount = 0;
+        for (let i = 0; i < e.factor.length; i++) {
+            let factor = e.factor[i];
+            let v = $(`#${factor.name}`).val()
+            let isValue = false
+            for (let j = 0; j < factor.value.length; j++) {
+                if (factor.value[j] == v) {
+                    isValue = true;
+                    break
+                }
+            }
+            if (isValue) {
+                factorCount++
+
+            }
+        }
+        if (factorCount == e.factor.length) {
+            $('#Strategy').text(e.name)
+            return false
+        } else {
+            return true
+        }
+
     })
 }
 
